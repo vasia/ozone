927cbdcbf6cfa121f15573a1482daba3208eaabf
vasia <vasilikikalavri@gmail.com>
2013-06-25 13:41:24 +0200

    added functionality for setting max iterations and the DataSink Contract

--- pact/pact-incremental/src/main/java/eu/stratosphere/pact/incremental/plans/BulkIterationPlan.java
+++ pact/pact-incremental/src/main/java/eu/stratosphere/pact/incremental/plans/BulkIterationPlan.java
@@ -10,6 +10,7 @@ import eu.stratosphere.pact.common.stubs.MatchStub;
 import eu.stratosphere.pact.common.stubs.ReduceStub;
 import eu.stratosphere.pact.common.type.Key;
 import eu.stratosphere.pact.generic.contract.Contract;
+import eu.stratosphere.pact.generic.io.OutputFormat;
 import eu.stratosphere.pact.incremental.contracts.BulkIterationContract;
 
 /**
@@ -40,22 +41,24 @@ public class BulkIterationPlan extends Plan implements BulkIterationPlanner {
 	private ReduceContract updateReduce;
 	private MatchContract comparisonMatch;
 
+	public BulkIterationPlan(GenericDataSink sink, String jobName) {
+		super(sink, jobName);
+		iteration = new BulkIterationContract(jobName);
+	}
+	
 	public BulkIterationPlan(GenericDataSink sink, String jobName, int keyPosition) {
 		super(sink, jobName);
 		iteration = new BulkIterationContract(keyPosition);
 	}
 
 	@Override
-	public void setUpBulkIteration(GenericDataSource<?> initialSolutionSet,
-			GenericDataSource<?> dependencySet) {
-
+	public void setUpBulkIteration(GenericDataSource<?> initialSolutionSet, GenericDataSource<?> dependencySet) {
 		iteration.setDependencySet(dependencySet);
 		iteration.setInitialSolutionSet(initialSolutionSet);	
 	}
 
 	@Override
-	public void setUpDependenciesMatch(Class<? extends MatchStub> udf,
-			Class<? extends Key> keyClass, int keyColumn1, int keyColumn2) {
+	public void setUpDependenciesMatch(Class<? extends MatchStub> udf, Class<? extends Key> keyClass, int keyColumn1, int keyColumn2) {
 		dependencyMatch = MatchContract.builder(udf, keyClass, keyColumn1, keyColumn2)
 				.input1(iteration.getPartialSolution())
 				.input2(iteration.getDependencySet())
@@ -85,6 +88,11 @@ public class BulkIterationPlan extends Plan implements BulkIterationPlanner {
 	@Override
 	public void assemble() {
 		iteration.setNextPartialSolution(comparisonMatch);		
+		this.getDataSinks().iterator().next().addInput(getIteration());
+	}
+	
+	public void setMaxIterations(int maxIterations){
+		iteration.setMaximumNumberOfIterations(maxIterations);
 	}
 	
 	public Contract getIteration() throws PlanException {
